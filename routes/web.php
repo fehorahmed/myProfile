978ec266e58fbfa904a070e1aa27ebcbab81390b
spirantfiend <asrana.edu@gmail.com>
2021-12-29 12:45:51 +0600

    auth

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\BannerPicController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,10 +17,17 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/',[HomeController::class,'home']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 
 
-Route::get('/admin/home',[AdminController::class,'index'])->name('admin.home');
+ Route::get('/',[HomeController::class,'home']);
+// Route::get('/admin/home',[AdminController::class,'index'])->name('admin.home');
+// Route::get('/admin/bannerpic',[BannerPicController::class,'index'])->name('banner.home');
 
-Route::get('/admin/bannerpic',[BannerPicController::class,'index'])->name('banner.home');
+
+
+require __DIR__.'/auth.php';
